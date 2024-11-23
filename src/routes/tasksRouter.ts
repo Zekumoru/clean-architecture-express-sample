c7a155463ef71b1680627b371c85e3f447e505d4
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-11-23 20:35:11 +0100

    Implement get task route

--- src/routes/tasksRouter.ts
+++ src/routes/tasksRouter.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import { getTasksController } from '../controllers/getTasksController';
 import { createTaskController } from '../controllers/createTaskController';
+import { getTaskController } from '../controllers/getTaskController';
 
 const tasksRouter = express.Router();
 
 tasksRouter.get('/', getTasksController);
 
 tasksRouter.post('/', createTaskController);
+
+tasksRouter.get('/:id', getTaskController);
 
 export default tasksRouter;
